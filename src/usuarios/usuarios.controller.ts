d8d01281b18769184e0c66537b73eb719a018168
RenanMMz <renan.m.mlol@gmail.com>
2025-11-20 22:09:45 -0300

    @put usuarios/update adicionado, recebe uma nova senha e rehasha antes de salvar no banco junto das outras informações re-encriptadas

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -7,10 +7,14 @@ import {
   Post,
   Put,
   NotFoundException,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUserDTO } from './dto/usuarios.dto';
 import { Usuario } from './usuarios.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UpdateUserDTO } from './dto/updateUsuarios.dto';
 
 @Controller('usuarios')
 export class UsuariosController {
@@ -20,4 +24,21 @@ export class UsuariosController {
   createUsuarios(@Body() createUserDTO: CreateUserDTO) {
     return this.usuariosService.createUsuarios(createUserDTO);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('/update')
+  async updateUsuarios(
+    @Req() req,
+    @Body() updateData: UpdateUserDTO,
+  ){
+    const userId = req.user.id;
+
+    await this.usuariosService.updateUser(
+      userId,
+      updateData
+    );
+
+    return { message: 'Senha e Vault atualizados com sucesso'};
+  }
+
 }
